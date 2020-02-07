2155ce99d9f3ffd3688c304e3178d8ba0832ea26
Alexander Bolshakov <alexander.bolshakov@sap.com>
2020-02-07 20:30:00 +0100

    Add publishing to the buld

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-pip install --user pylint
+pip install --user pylint twine pydoc-markdown
 pylint sap-document-classification-client/
-
-pip install --user pydoc-markdown
 pydocmd simple sap-document-classification-client+ sap-document-classification-client.dc_api_client++ > API.md
+python setup.py sdist
+twine upload dist/*
