352a2bd6436ec44dd7b3456a0ee9f82fb5bbaa11
D070760 <till.geissler@sap.com>
2021-04-16 09:54:16 +0200

    remove comment from sap_business_document_processing/__init__.py

--- sap_business_document_processing/__init__.py
+++ sap_business_document_processing/__init__.py
@@ -2,7 +2,5 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-# Best practice to provide main classes on top level?
-# Could decide on different approach, see: https://docs.python-guide.org/writing/structure/ or https://pythonpackaging.info/02-Package-Structure.html
 from .document_classification_client.dc_api_client import DCApiClient
 from .document_information_extraction_client.dox_api_client import DoxApiClient
